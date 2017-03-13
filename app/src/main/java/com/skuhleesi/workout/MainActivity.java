1b8805afdf99df689ac9ab87b5774ee644deda35
AndrewScalise <paradigmcomplex@gmail.com>
2017-03-13 14:36:18 -0500

    Create workout list fragement

--- app/src/main/java/com/skuhleesi/workout/MainActivity.java
+++ app/src/main/java/com/skuhleesi/workout/MainActivity.java
@@ -3,11 +3,16 @@ package com.skuhleesi.workout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements WorkoutListFragment.WorkoutListListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void itemClicked(long id){
+        //Method is defined in the listener
     }
 }
